5170e4241bb2855602a8e9d43aa05ab3cc35367d
OriCohen05 <79476840+OriCohen05@users.noreply.github.com>
2024-07-11 15:39:24 +0300

    Board html

--- model/Board.js
+++ model/Board.js
@@ -1,34 +1,42 @@
-const {BOARD_GRID_COLUMNS, BOARD_GRID_ROWS} = require('../config/Constants');
-const Cell = require('./Cell');
-const Shape = require('./Shape');
+const { BOARD_GRID_COLUMNS, BOARD_GRID_ROWS } = require("../config/Constants");
+const Cell = require("./Cell");
 
+class Board {
+  constructor() {
+    this.board = [];
 
-class Board{
-    constructor() {
-        this.board = [];
-
-        for (let i = 0; i < BOARD_GRID_COLUMNS; i++) {
-          this.board[i] = [];
-          for (let j = 0; j < BOARD_GRID_ROWS; j++) {
-            this.board[i][j] = new Cell();
-          }
-        }
-    }
-
-    isFull() {
-        for (let row of this.board) {
-          for (let cell of row) {
-            if (cell.emptyCell) {
-              return false;
-            }
-          }
-        }
-        return true;
+    for (let i = 0; i < BOARD_GRID_ROWS; i++) {
+      this.board[i] = [];
+      for (let j = 0; j < BOARD_GRID_COLUMNS; j++) {
+        this.board[i][j] = new Cell();
       }
+    }
+  }
+
+  isFull() {
+    for (let row of this.board) {
+      for (let cell of row) {
+        if (cell.emptyCell) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  #CanPlay(row, column){
+    return this.board[row][column].emptyCell;
+  }
+
+  TryPlay(row, column, shape) {
+    if(!this.#CanPlay(row, column)) return false;
+    this.board[row][column].emptyCell = false;
+    this.board[row][column].shape = shape;
+    return true;
+  }
+
+  
 
 }
-
-b = new Board();
-console.log(b.isFull());
 
 module.exports = Board;
